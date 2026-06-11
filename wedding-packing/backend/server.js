07837d1a22c2d11d1f997a85adffdad76242ffe8
Kanishk Sharma <thekanishk2005@gmail.com>
2026-06-11 15:00:58 +0530

    Update server.js

--- wedding-packing/backend/server.js
+++ wedding-packing/backend/server.js
@@ -6,7 +6,15 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({
+  origin: [
+    'https://wedding-packing-site-frontend.onrender.com',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
